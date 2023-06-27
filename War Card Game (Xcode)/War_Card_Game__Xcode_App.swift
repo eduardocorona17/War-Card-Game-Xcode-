18ec7a612aac68ab4e8f8b9229c330dcd5fbd0c9
Eduardo Corona <ecoronalopez@gmail.com>
2023-06-26 18:21:48 -0700

    Initial Commit

---
+++ War Card Game (Xcode)/War_Card_Game__Xcode_App.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_Game__Xcode_App.swift
+//  War Card Game (Xcode)
+//
+//  Created by Eduardo Corona on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_Game__Xcode_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
